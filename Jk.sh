138e59985d84d59e2dc8a4def139a5696d65e249
mandelin007 <60238137+mandelin007@users.noreply.github.com>
2020-06-04 21:55:36 +0530

    Update Jk.sh

--- Jk.sh
+++ Jk.sh
@@ -3,3 +3,4 @@ echo ########search metasploit########
 echo ########start database for metasploit########
 pg_ctl -D $PREFIX/var/lib/postgresql start
 echo ########start metasploit framework########
+./msfconsole
