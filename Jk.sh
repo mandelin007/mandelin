4576722068179a8a9ce8b94ecbc5c61b4f1b2056
mandelin007 <60238137+mandelin007@users.noreply.github.com>
2020-06-04 21:51:34 +0530

    Create Jk.sh

---
+++ Jk.sh
@@ -0,0 +1,5 @@
+echo ########search metasploit########
+cd metasploit-framework
+echo ########start database for metasploit########
+pg_ctl -D $PREFIX/var/lib/postgresql start
+echo ########start metasploit framework########
