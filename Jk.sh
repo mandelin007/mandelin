de0f4bf1ca774a2e09c7d90289ffb98c44978f4a
mandelin007 <60238137+mandelin007@users.noreply.github.com>
2020-07-13 09:55:12 +0530

    Update Jk.sh

--- Jk.sh
+++ Jk.sh
@@ -1,6 +1,32 @@
-echo ########search metasploit########
- cd metasploit-framework
-echo ########start database for metasploit########
+echo "########search metasploit########"
+cd metasploit-framework
+echo "########start database for metasploit########" >
 pg_ctl -D $PREFIX/var/lib/postgresql start
-echo ########start metasploit framework########
+echo "########start metasploit-framework########"
+
+echo "       .:okOOOkdc'           'cdkOOOko:.
+    .xOOOOOOOOOOOOc       cOOOOOOOOOOOOx.
+   :OOOOOOOOOOOOOOOk,   ,kOOOOOOOOOOOOOOO:
+  'OOOOOOOOOkkkkOOOOO: :OOOOOOOOOOOOOOOOO0;
+  oOOOOOOOO.    .oOOOOoOOOOl.    ,OOOOOOOOo
+  dOOOOOOOO.      .cOOOOOc.      ,OOOOOOOOx
+  lOOOOOOOO.         ;d;         ,OOOOOOOOl
+  .OOOOOOOO.   .;           ;    ,OOOOOOOO.
+   cOOOOOOO.   .OOc.     'oOO.   ,OOOOOOOc
+    oOOOOOO.   .OOOO.   :OOOO.   ,OOOOOOo
+     lOOOOO.   .OOOO.   :OOOO.   ,OOOOOl
+      ;OOOO'   .OOOO.   :OOOO.   ;OOOO;
+       .dOOo   .OOOOocccxOOOO.   xOOd.
+         ,kOl  .OOOOOOOOOOOOO. .dOk,
+           :kk;.OOOOOOOOOOOOO.cOk:
+             ;kOOOOOOOOOOOOOOOk:
+               ,xOOOOOOOOOOOx,
+                 .lOOOOOOOl.
+                    ,dOd,
+                      .  "
 ./msfconsole
+
+
+
+
+
